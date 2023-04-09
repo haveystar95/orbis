743f909e217146b241907744cef128195b2fc354
denis-solonina <dsolonyna@axon.dev>
2023-04-09 22:15:12 +0300

    v0.3 -PortfolioService Main logic getting value

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,14 +7,14 @@ adduser -S myuser -G mygroup
 # Set ownership of the application directory
 chown -R myuser:mygroup /var/www
 chown -R myuser:mygroup /var/www/storage
-chmod -R 775 /var/www/storage
+chmod -R 777 /var/www/storage
 
 # Install composer dependencies
 cd /var/www
 su-exec myuser composer install
 su-exec myuser php artisan key:generate
 su-exec myuser php artisan migrate
-su-exec myuser php artisan import:historical-data path/to/historical_data.csv
+su-exec myuser php artisan import:historical-data storage/app/historical_data.csv
 
 exec php-fpm
 
