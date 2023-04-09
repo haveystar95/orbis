5775617d5679a288ce12af5554a3a4124159643e
denis-solonina <dsolonyna@axon.dev>
2023-04-09 21:44:22 +0300

    v0.3 -PortfolioService Main logic getting value

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+echo "sdfsdf"
 # Create a new user and group
 addgroup -S mygroup
 adduser -S myuser -G mygroup
@@ -9,9 +9,10 @@ chown -R myuser:mygroup /var/www
 
 # Install composer dependencies
 cd /var/www
-sudo -u myuser composer install
-sudo -u myuser php artisan key:generate
-sudo -u myuser php artisan migrate
+su-exec myuser composer install
+su-exec myuser php artisan key:generate
+su-exec myuser php artisan migrate
+
+exec php-fpm
 
 # Start the PHP-FPM process
-exec php-fpm
